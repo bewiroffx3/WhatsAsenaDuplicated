9de5cab287cfd878a2e1ce43be12d198dfb4162c
Bewiroff <85064556+bewiroff44@users.noreply.github.com>
2021-08-11 01:03:49 +0400

    Create antispam.js

---
+++ plugins/antispam.js
@@ -0,0 +1,71 @@
+const Asena = require('../events');
+
+const {MessageType} = require('@adiwajshing/baileys');
+
+const Ln = ('This command for any emergency situation about any kind of WhatsApp SPAM in Group');
+
+const code = ('A\nɴ\nᴛ\nɪ\ns\nᴘ\nᴀ\nᴍ\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n *🎭Bewiroff*')
+const FINAL = "```⚠️Bu bir antispm (gecikme önleyici) Bu kod, telefonunuzun kötü amaçlı mesajlar nedeniyle sıkışmasını önler.⚠️```"
+const TMUTE = "Geçici olarak kapatılmaya çalışılıyor,\nGrubu geçici olarak kapatılmaya çalışılıyor"
+const MUT = ".mute 1d"
+const SCXR = "```♻️Komut Başlatılıyor...♻️```"
+Asena.addCommand({pattern: 'antispm', fromMe: true, deleteCommand: false, desc: Ln,}, (async (message, match) => {
+
+  var msg = await message.reply('```☢Ali Bey Emrinle Grup Tehlikeden Kurtuluyor....☢```');
+
+  await message.client.sendMessage(
+    message.jid,TMUTE, MessageType.text);
+
+    await message.client.sendMessage(
+      message.jid,MUT, MessageType.text);
+
+      await message.client.sendMessage(
+        message.jid,SCXR, MessageType.text);
+
+        await message.client.sendMessage(
+          message.jid,code, MessageType.text);
+
+    await message.client.sendMessage(
+        message.jid,code, MessageType.text);
+
+        await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                               await message.client.sendMessage(
+            message.jid,code, MessageType.text);
+
+            await message.client.sendMessage(
+                message.jid,code, MessageType.text);
+
+                await message.client.sendMessage(
+                  message.jid,code, MessageType.text);
+
+                  await message.client.sendMessage(
+                    message.jid,code, MessageType.text);
+
+                    await message.client.sendMessage(
+                      message.jid,code, MessageType.text);
+
+                      await message.client.sendMessage(
+                        message.jid,code, MessageType.text);
+  
+                          await message.client.sendMessage(
+                             message.jid,FINAL, MessageType.text);
+
+
+}));
